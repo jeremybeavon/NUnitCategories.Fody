995302260cfed1a5b349c4f7d7dc68ecc39278ee
jeremybeavon <jeremybeavon@users.noreply.github.com>
2016-05-27 11:35:24 +1200

    Add ProcessorCount functionality

--- NUnitCategories.Fody/ModuleWeaver.cs
+++ NUnitCategories.Fody/ModuleWeaver.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Linq;
 using Mono.Cecil;
-using Mono.Collections.Generic;
 
 namespace NUnitCategories.Fody
 {
@@ -16,14 +13,24 @@ namespace NUnitCategories.Fody
 
         public Action<string> LogError { get; set; }
 
+        public Action<string> LogWarning { get; set; }
+
+        public Action<string> LogDebug { get; set; }
+
+        public XElement Config { get; set; }
+
         public ModuleWeaver()
         {
             categoryAttributeConstructor = new Lazy<MethodDefinition>(FindCategoryAttributeConstructor);
             LogError = message => { };
+            LogWarning = message => { };
+            LogDebug = message => { };
         }
 
         public void Execute()
         {
+            int? processorCount = ReadProcessorCount();
+            int testCount = 0;
             foreach (TypeDefinition type in ModuleDefinition.Types.Where(type => HasAttribute(type, "TestFixtureAttribute")))
             {
                 string typeName = type.Namespace + "." + type.Name;
@@ -33,6 +40,11 @@ namespace NUnitCategories.Fody
                 {
                     AddCategoryAttribute(method, typeName + "." + method.Name);
                 }
+
+                if (processorCount.HasValue)
+                {
+                    AddCategoryAttribute(type, "CPU_" + (testCount++ % processorCount.Value + 1));
+                }
             }
         }
 
@@ -41,9 +53,40 @@ namespace NUnitCategories.Fody
             return provider.CustomAttributes.Any(attribute => attribute.AttributeType.Name == attributeName);
         }
 
+        private int? ReadProcessorCount()
+        {
+            if (Config == null)
+            {
+                LogDebug("ModuleWeaver.Config is null");
+                return null;
+            }
+
+            XAttribute attribute = Config.Attribute("ProcessorCount");
+            if (attribute == null)
+            {
+                LogDebug("Fody configuration for NUnitCategories does not contain a ProcessorCount attribute.");
+                return null;
+            }
+
+            int processorCount;
+            if (!int.TryParse(attribute.Value, out processorCount))
+            {
+                LogWarning("Fody configuration for NUnitCategories contains a ProcessorCount attribute but it is not an integer.");
+                return null;
+            }
+
+            return processorCount;
+        }
+
         private void AddCategoryAttribute(ICustomAttributeProvider provider, string categoryName)
         {
-            CustomAttribute categoryAttribute = new CustomAttribute(ModuleDefinition.Import(categoryAttributeConstructor.Value));
+            MethodDefinition constructor = categoryAttributeConstructor.Value;
+            if (constructor == null)
+            {
+                return;
+            }
+
+            CustomAttribute categoryAttribute = new CustomAttribute(ModuleDefinition.Import(constructor));
             categoryAttribute.ConstructorArguments.Add(new CustomAttributeArgument(ModuleDefinition.TypeSystem.String, categoryName));
             provider.CustomAttributes.Add(categoryAttribute);
         }
@@ -54,6 +97,7 @@ namespace NUnitCategories.Fody
             if (nunitAssembly == null)
             {
                 LogError("nunit.framework could not be found.");
+                return null;
             }
 
             TypeDefinition categoryAttributeType = nunitAssembly.MainModule.Types.FirstOrDefault(
@@ -61,16 +105,17 @@ namespace NUnitCategories.Fody
             if (categoryAttributeType == null)
             {
                 LogError("CategoryAttribute type could not be found.");
+                return null;
             }
 
-            MethodDefinition categoryAttributeConstructor = categoryAttributeType.Methods.FirstOrDefault(
+            MethodDefinition constructor = categoryAttributeType.Methods.FirstOrDefault(
                 method => method.IsConstructor && method.Parameters.Count == 1);
-            if (categoryAttributeConstructor == null)
+            if (constructor == null)
             {
                 LogError("CategoryAttribute constructor could not be found.");
             }
 
-            return categoryAttributeConstructor;
+            return constructor;
         }
     }
 }
